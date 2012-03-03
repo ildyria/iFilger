d8576d9118142dfc33da0c76aac7acb3e0dc289c
ildyria <bvwebmaster3000-bn@yahoo.fr>
2012-03-04 00:06:38 +0100

    WIP...

--- iFilger_ConfigUI/Config/module.lua
+++ iFilger_ConfigUI/Config/module.lua
@@ -4,7 +4,7 @@ local C, F, L = unpack(select(2, ...))
 --													TODO LIST
 --
 --	/!\ /!\	add saved variables (mandatory) http://www.wowwiki.com/Saving_variables_between_game_sessions
---	/!\ /!\	Reset button so we can go back to initial config
+--	/!\ /!\	Reset button so we can go back to initial config -- DONE (IN THEORY...)
 --	  /!\	add Enable
 --	  /!\	add Size
 --	  /!\	add Direction
@@ -25,42 +25,39 @@ local C, F, L = unpack(select(2, ...))
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 --local ipairs = ipairs
-local class = select(2, UnitClass("player"));
+local myPlayerRealm = GetCVar("realmName")
+local myPlayerName  = UnitName("player")
+local myClass = select(2, UnitClass("player"));
 local tabs = {  											-- List of headers and number of tabs inside each ones
 	[1] = {
-		name = class,										--	"MAGE","DEATHKNIGHT","PRIEST";"WARLOCK","DRUID","HUNTER","ROGUE","PALADIN","SHAMAN","WARRIOR","HUNTER/DRUID/ROGUE",
-		numtab = 0,											-- number of tab in the header (is it realy usefull ??? O.o)
+		name = myClass,										--	"MAGE","DEATHKNIGHT","PRIEST";"WARLOCK","DRUID","HUNTER","ROGUE","PALADIN","SHAMAN","WARRIOR","HUNTER/DRUID/ROGUE",
 		tablist = {},										-- list of tabs => to know which ones to open and close
 	},
 	[2] = {
 		name = "HUNTER/DRUID/ROGUE",
-		numtab = 0,
 		tablist = {},
 	},
 	[3] = {
 		name = "ALL",
-		numtab = 0,
 		tablist = {},
 	},
 	[4] = {
 		name = "PVP",
-		numtab = 0,
 		tablist = {},
 	},
 	[5] = {
 		name = "PVE",
-		numtab = 0,
 		tablist = {},
 	},
 	[6] = {
 		name = "TANKS",
-		numtab = 0,
 		tablist = {},
 	},
 }
 local header = {}											-- List of headers (frames)
 local tab = {}												-- List of tab (frames)
 local options = {}											-- List of options (frames)
+C["tempdata"] = {}											-- List of savedvariables => easier to work with
 
 local iFilger_Spells										-- list of spells we want to modify
 if iFilgerConfig then 
@@ -75,11 +72,11 @@ local create_panel = function ()
 	local iFilgerconfigPanel = CreateFrame("Frame", "iFilgerConfig", UIParent)
 	F.CreatePanel(iFilgerconfigPanel, 300, 500, "CENTER", UIParent, "CENTER", 0, 140)
 	F.SetBorder(iFilgerconfigPanel)
-	iFilgerconfigPanel:EnableMouse(true)
-	iFilgerconfigPanel:SetMovable(true)
-	iFilgerconfigPanel:RegisterForDrag("LeftButton")
-	iFilgerconfigPanel:SetScript("OnDragStart", function(self) self:SetUserPlaced(true) self:StartMoving() end)
-	iFilgerconfigPanel:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+--	iFilgerconfigPanel:EnableMouse(true)
+--	iFilgerconfigPanel:SetMovable(true)
+--	iFilgerconfigPanel:RegisterForDrag("LeftButton")
+--	iFilgerconfigPanel:SetScript("OnDragStart", function(self) self:SetUserPlaced(true) self:StartMoving() end)
+--	iFilgerconfigPanel:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
 	iFilgerconfigPanel:Hide()
 
 	local configTitle = CreateFrame("Frame", "iFilgerConfigTitle", iFilgerconfigPanel)
@@ -105,8 +102,68 @@ local create_panel = function ()
 	configClose.text:SetFont(C.font, 14)
 	configClose.text:SetText("X")
 	configClose.text:SetPoint("CENTER", configClose, 0, 0)
+
+	local configApply = CreateFrame("Frame", "iFilgerConfigApply", iFilgerconfigPanel)
+	F.CreatePanel(configApply, 95, 25, "TOPRIGHT", iFilgerconfigPanel, "BOTTOMRIGHT", 0, -5)
+	configApply:SetFrameStrata("MEDIUM")
+	configApply:EnableMouse(true)
+	configApply:SetScript("OnEnter", function(self) self.text:SetTextColor(1,0,0) end)
+	configApply:SetScript("OnLeave", function(self) self.text:SetTextColor(1,1,1) end)
+--	configApply:SetScript("OnMouseDown", function() iFilgerconfigPanel:Hide() end)
+	configApply:SetScript("OnClick", function(self) ReloadUI() end)
+
+	configApply.text = configApply:CreateFontString(nil, "OVERLAY")
+	configApply.text:SetFont(C.font, 14)
+	configApply.text:SetText("Apply")
+	configApply.text:SetPoint("CENTER", configApply, 0, -1)
+
+	local configReset = CreateFrame("Frame", "iFilgerConfigReset", iFilgerconfigPanel)
+	F.CreatePanel(configReset, 95, 25, "TOPLEFT", iFilgerconfigPanel, "BOTTOMLEFT", 0, -5)
+	configReset:SetFrameStrata("MEDIUM")
+	configReset:EnableMouse(true)
+	configReset:SetScript("OnEnter", function(self) self.text:SetTextColor(1,0,0) end)
+	configReset:SetScript("OnLeave", function(self) self.text:SetTextColor(1,1,1) end)
+--	configReset:SetScript("OnMouseDown", function() iFilgerconfigPanel:Hide() end)
+	configReset:SetScript("OnClick", function(self) 
+		ToggleFrame(iFilgerconfigPanel)
+		StaticPopup_Show("RESET_IFILGER")
+	end)
+	
+	configReset.text = configReset:CreateFontString(nil, "OVERLAY")
+	configReset.text:SetFont(C.font, 14)
+	configReset.text:SetText("Reset Config")
+	configReset.text:SetPoint("CENTER", configReset, 0, -1)
+	
+	local configMove = CreateFrame("Button", "iFilgerConfigMove", iFilgerconfigPanel, "SecureActionButtonTemplate")
+	F.CreatePanel(configMove, 95, 25, "TOP", iFilgerconfigPanel, "BOTTOM", 0, -5)
+	configMove:EnableMouse(true)
+	configMove:RegisterForClicks("AnyUp")
+	configMove:SetAttribute("type1", "macro")
+	configMove:SetAttribute("macrotext1", "/mifilger")
+	configMove:SetFrameStrata("MEDIUM")
+	configMove:SetScript("OnEnter", function(self) self.text:SetTextColor(1,0,0) end)
+	configMove:SetScript("OnLeave", function(self) self.text:SetTextColor(1,1,1) end)
+	configMove:SetScript("OnMouseDown", function() ToggleFrame(iFilgerconfigPanel) end)
+
+	configMove.text = configMove:CreateFontString(nil, "OVERLAY")
+	configMove.text:SetFont(C.font, 14)
+	configMove.text:SetText("Move Config")
+	configMove.text:SetPoint("CENTER", configMove, 0, -1)
 end
 
+StaticPopupDialogs["RESET_IFILGER"] = {
+	text = "Are you sure you want to reset all of your settings to Default?",
+	OnAccept = function() 
+		iFilgerConfigUISavedVariables = nil
+		ReloadUI() 
+	end,
+	OnCancel = function() ToggleFrame(iFilgerconfigPanel) end,
+	button1 = ACCEPT,
+	button2 = CANCEL,
+	timeout = 0,
+	whileDead = 1,
+	preferredIndex = 3,
+}
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------
@@ -174,6 +231,7 @@ local create_tab = function(data,indicetab,j,i)
 	options[indicetab].Enable.button = CreateFrame("CheckButton", "iFilgerConfigEnable"..indicetab, iFilgerconfigPanel, "ChatConfigCheckButtonTemplate")
 	options[indicetab].Enable.button:SetPoint("RIGHT", options[indicetab].Enable, "LEFT", F.Scale(-5), F.Scale(-1))
 --	options[indicetab].Enable.button:SetBackdropBorderColor(unpack(C["General"].BorderColor)) - to test later
+	options[indicetab].Enable.button:SetChecked(false)
 
 	-- options : Size
 	options[indicetab].Size = options[indicetab]:CreateFontString(nil, "OVERLAY")
@@ -295,11 +353,12 @@ create_panel()
 
 local data = {}
 local indicetab = 0
+local numtab
 for j = 1, #tabs, 1 do
-	if tabs[j].name ~= "HUNTER/DRUID/ROGUE" or ((class == "HUNTER" or class == "DRUID" or class == "ROGUE") and tabs[j].name == "HUNTER/DRUID/ROGUE") then
+	if tabs[j].name ~= "HUNTER/DRUID/ROGUE" or ((myClass == "HUNTER" or myClass == "DRUID" or myClass == "ROGUE") and tabs[j].name == "HUNTER/DRUID/ROGUE") then
 		create_tab_head(j,tabs[j].name)							-- create tab header
-		tabs[j].numtab = #iFilger_Spells[tabs[j].name]			-- get number of tab in the header
-		for i = 1, tabs[j].numtab, 1 do
+		numtab = #iFilger_Spells[tabs[j].name]			-- get number of tab in the header
+		for i = 1, numtab, 1 do
 			indicetab = indicetab + 1							-- increase tab id
 			data = iFilger_Spells[tabs[j].name][i]				-- get data about the current tab
 			create_tab(data,indicetab,j,i)						-- create tab and options
@@ -316,6 +375,121 @@ for j = 2, #tabs, 1 do
 end
 
 
+---------------------------------------------------------------------------------------------------------------------------------------
+--	LOAD SAVED VARIABLES
+---------------------------------------------------------------------------------------------------------------------------------------
+
+-- Saved Variables
+local SaveVariables = function(self, event)
+	local data = {}
+	local indicetab = 0
+	local numtab
+	if event == "ADDON_LOADED" then
+		if iFilgerConfigUISavedVariables == nil then	-- there are no saved variables, let's load the default config ! 
+			iFilgerConfigUISavedVariables = {}			-- let's create an empty table
+
+			for j = 1, #tabs, 1 do
+				if tabs[j].name ~= "HUNTER/DRUID/ROGUE" or ((myClass == "HUNTER" or myClass == "DRUID" or myClass == "ROGUE") and tabs[j].name == "HUNTER/DRUID/ROGUE") then
+					numtab = #iFilger_Spells[tabs[j].name]			-- get number of tab in the header
+
+					for i = 1, numtab, 1 do
+						indicetab = indicetab + 1							-- increase tab id
+						data = iFilger_Spells[tabs[j].name][i]				-- get data about the current tab
+
+						iFilgerConfigUISavedVariables[indicetab].Name = data.Name
+						iFilgerConfigUISavedVariables[indicetab].Direction = data.Direction or "UP"
+						iFilgerConfigUISavedVariables[indicetab].IconSide = data.IconSide or "LEFT"
+						iFilgerConfigUISavedVariables[indicetab].Interval = data.Interval or 3
+						iFilgerConfigUISavedVariables[indicetab].Mode = data.Mode or "ICON"
+						iFilgerConfigUISavedVariables[indicetab].Size = data.Size or 20
+						iFilgerConfigUISavedVariables[indicetab].Alpha = data.Alpha or 1.0
+						iFilgerConfigUISavedVariables[indicetab].BarWidth = data.BarWidth or 200
+						iFilgerConfigUISavedVariables[indicetab].setPoint = data.setPoint or "CENTER"
+					end
+
+				end
+			end
+
+		end
+		
+		indicetab = 0
+		if iFilgerConfigUISavedVariables then  -- there are saved variables, let's load them !
+		-- Put this will overwrite profiles.
+			for j = 1, #tabs, 1 do
+				if tabs[j].name ~= "HUNTER/DRUID/ROGUE" or ((myClass == "HUNTER" or myClass == "DRUID" or myClass == "ROGUE") and tabs[j].name == "HUNTER/DRUID/ROGUE") then
+					numtab = #iFilger_Spells[tabs[j].name]			-- get number of tab in the header
+
+					for i = 1, numtab, 1 do
+						indicetab = indicetab + 1									-- increase tab id
+						data = iFilgerConfigUISavedVariables[indicetab]				-- get data about the current tab
+
+						iFilger_Spells[tabs[j].name][i].Name = data.Name
+						iFilger_Spells[tabs[j].name][i].Direction = data.Direction or "UP"
+						iFilger_Spells[tabs[j].name][i].IconSide = data.IconSide or "LEFT"
+						iFilger_Spells[tabs[j].name][i].Interval = data.Interval or 3
+						iFilger_Spells[tabs[j].name][i].Mode = data.Mode or "ICON"
+						iFilger_Spells[tabs[j].name][i].Size = data.Size or 20
+						iFilger_Spells[tabs[j].name][i].Alpha = data.Alpha or 1.0
+						iFilger_Spells[tabs[j].name][i].Barwidth = data.BarWidth or 200
+						iFilger_Spells[tabs[j].name][i].setPoint = data.setPoint or "CENTER"
+						
+						C["tempdata"][indicetab].Name = data.Name
+						C["tempdata"][indicetab].Direction = data.Direction or "UP"
+						C["tempdata"][indicetab].IconSide = data.IconSide or "LEFT"
+						C["tempdata"][indicetab].Interval = data.Interval or 3
+						C["tempdata"][indicetab].Mode = data.Mode or "ICON"
+						C["tempdata"][indicetab].Size = data.Size or 20
+						C["tempdata"][indicetab].Alpha = data.Alpha or 1.0
+						C["tempdata"][indicetab].Barwidth = data.BarWidth or 200
+						C["tempdata"][indicetab].setPoint = data.setPoint or "CENTER"
+					end
+
+				end
+			end
+		end
+		
+	elseif event == "PLAYER_LOGOUT" then
+		for i = 1, #C["tempdata"], 1 do
+			data = C["tempdata"][i]			-- get data about the current tab
+
+			iFilgerConfigUISavedVariables[i].Name = data.Name
+			iFilgerConfigUISavedVariables[i].Direction = data.Direction or "UP"
+			iFilgerConfigUISavedVariables[i].IconSide = data.IconSide or "LEFT"
+			iFilgerConfigUISavedVariables[i].Interval = data.Interval or 3
+			iFilgerConfigUISavedVariables[i].Mode = data.Mode or "ICON"
+			iFilgerConfigUISavedVariables[i].Size = data.Size or 20
+			iFilgerConfigUISavedVariables[i].Alpha = data.Alpha or 1.0
+			iFilgerConfigUISavedVariables[i].BarWidth = data.BarWidth or 200
+			iFilgerConfigUISavedVariables[i].setPoint = data.setPoint or "CENTER"
+		end
+	end
+	
+	-- And now we set all vars in config panel ! 
+	for i = 1, #C["tempdata"], 1 do
+		data = C["tempdata"][i]			-- get data about the current tab
+		
+		tab[indicetab].text:SetText(data.Name)										-- set tab name (even if supposed already done)
+		options[indicetab].Size.Editbox:SetText(data.Size)							-- set Size
+		if data.Enable	then options[indicetab].Enable.button:SetChecked() end		-- set Enable
+
+
+		-- iFilgerConfigUISavedVariables[i].Name = data.Name
+		-- iFilgerConfigUISavedVariables[i].Direction = data.Direction or "UP"
+		-- iFilgerConfigUISavedVariables[i].IconSide = data.IconSide or "LEFT"
+		-- iFilgerConfigUISavedVariables[i].Interval = data.Interval or 3
+		-- iFilgerConfigUISavedVariables[i].Mode = data.Mode or "ICON"
+		-- iFilgerConfigUISavedVariables[i].Size = data.Size or 20
+		-- iFilgerConfigUISavedVariables[i].Alpha = data.Alpha or 1.0
+		-- iFilgerConfigUISavedVariables[i].BarWidth = data.BarWidth or 200
+		-- iFilgerConfigUISavedVariables[i].setPoint = data.setPoint or "CENTER"
+	end
+end
+
+local saver = CreateFrame("FRAME")
+saver:RegisterEvent("ADDON_LOADED")
+saver:RegisterEvent("PLAYER_LOGOUT")
+saver:SetScript("OnEvent", SaveVariables)
+
 
 ---------------------------------------------------------------------------------------------------------------------------------------
 --	TOOGLE FUNCTION + SLASH COMMAND
@@ -327,5 +501,5 @@ local EnableConfig = function()
 	end
 end
 
-SLASH_IFILGERCONFIG1 = "/ciFilger"										-- slash command will need some rework later i guess... :/
+SLASH_IFILGERCONFIG1 = "/iFilger"										-- slash command will need some rework later i guess... :/
 SlashCmdList["IFILGERCONFIG"] = EnableConfig
